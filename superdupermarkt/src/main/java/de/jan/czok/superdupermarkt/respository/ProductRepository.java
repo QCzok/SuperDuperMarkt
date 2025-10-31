5a82b8d99b6aa728dfa923fae896c3098e98242c
Jan Czok <j.czok@svg.de>
2025-10-31 10:41:08 +0100

    Revert "Add files via upload"
    This reverts commit a23a5fcce3342f0c406386c7681fe09f1cf273c4.

--- superdupermarkt/src/main/java/de/jan/czok/superdupermarkt/respository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package de.jan.czok.superdupermarkt.respository;
-
-import de.jan.czok.superdupermarkt.model.Product;
-
-import java.util.List;
-
-public interface ProductRepository {
-    List<Product> findAll();
-}
